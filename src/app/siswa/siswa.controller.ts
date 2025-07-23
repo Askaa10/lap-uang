b940ce671d2710340ccdcc1f7195e2aada06644a
fawwaznwfl <nwflfawwaz@gmail.com>
2025-07-23 13:30:51 +0700

    be student

--- src/app/siswa/siswa.controller.ts
+++ src/app/siswa/siswa.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Param,
+    Put,
+    Delete,
+  } from '@nestjs/common';
 
-@Controller('siswa')
-export class SiswaController {}
+import { CreateStudentDto, UpdateStudentDto } from './siswa.dto';
+import { SiswaService } from './siswa.service';
+  
+  @Controller('students')
+  export class SiswaController {
+    constructor(private readonly siswaService: SiswaService) {}
+  
+    @Post('tambah-siswa')
+    create(@Body() dto: CreateStudentDto) {
+      return this.siswaService.create(dto);
+    }
+  
+    @Get('daftar-siswa')
+    findAll() {
+      return this.siswaService.findAll();
+    }
+  
+    @Get('detail/:id')
+    findOne(@Param('id') id: string) {
+      return this.siswaService.findOne(id);
+    }
+  
+    @Put('update/:id')
+    update(@Param('id') id: string, @Body() dto: UpdateStudentDto) {
+      return this.siswaService.update(id, dto);
+    }
+  
+    @Delete('hapus/:id')
+    remove(@Param('id') id: string) {
+      return this.siswaService.remove(id);
+    }
+  }
+  
