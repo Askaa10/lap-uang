a3bce62808044ae3f44825c7c45210d2beb08a38
Askaa10 <bdmaaan7@gmail.com>
2025-07-25 09:42:47 +0700

    ok

--- src/app/report/report.controller.ts
+++ src/app/report/report.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ReportService } from './report.service';
+import { CreateSppPaymentDto, ReportQueryDto } from './report-query.dto';
 
 @Controller('report')
-export class ReportController {}
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+
+  @Get('spp')
+  async getSppReport(@Query() query: ReportQueryDto) {
+    return this.reportService.getSppReport(query);
+  }
+
+  @Get('lainnya')
+  async getOtherPayments() {
+    return this.reportService.getOtherPayments();
+  }
+
+  @Get('tunggakan')
+  async getTunggakanReport() {
+    return this.reportService.getTunggakanReport();
+  }
+}
