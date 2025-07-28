90bbabf0433936b497124976bec4be03b8295273
fawwaznwfl <nwflfawwaz@gmail.com>
2025-07-28 11:55:35 +0700

    be

--- src/app/report/report.controller.ts
+++ src/app/report/report.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ReportService } from './report.service';
 
 @Controller('report')
-export class ReportController {}
+export class ReportController {
+        constructor(private readonly reportService: ReportService) {}
+        
+        
+  @Get('ppus')
+  getRekap() {
+    return this.reportService.getRekapPembayaran();
+  }
+
+  @Get('trpus')
+getRekapTagihan() {
+  return this.reportService.getRekapTagihan();
+}
+
+    
+@Get('tpus')
+getRekapKekurangan() {
+  return this.reportService.getRekapKekurangan();
+}
+
+}
+
