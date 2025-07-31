2eb8ace58a0bf3200d5c5bc0dac0f9d873d0e246
fawwaznwfl <nwflfawwaz@gmail.com>
2025-07-31 08:09:53 +0700

    be hari ini

--- src/app/payment/payment.controller.ts
+++ src/app/payment/payment.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { PaymentService } from './payment.service';
+import { CreatePaymentDto, UpdatePaymentDto } from './payment.dto';
+@Controller('payments')
+export class PaymentController {
+  constructor(private readonly paymentService: PaymentService) {}
 
-@Controller('payment')
-export class PaymentController {}
+  @Post('tambah')
+  create(@Body() dto: CreatePaymentDto) {
+    return this.paymentService.create(dto);
+  }
+
+  @Get('semua')
+  findAll() {
+    return this.paymentService.findAll();
+  }
+
+  @Get('detail/:id')
+  findOne(@Param('id') id: string) {
+    return this.paymentService.findOne(id);
+  }
+
+  @Patch('update/:id')
+  update(@Param('id') id: string, @Body() dto: UpdatePaymentDto) {
+    return this.paymentService.update(id, dto);
+  }
+
+  @Delete('delete/:id')
+  remove(@Param('id') id: string) {
+    return this.paymentService.remove(id);
+  }
+}
