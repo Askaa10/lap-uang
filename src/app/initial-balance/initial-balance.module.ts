afdc65a7407db43d5d4beb498fee52b122af4a04
Askaa10 <bdmaaan7@gmail.com>
2025-08-06 11:18:03 +0700

    oaksd

--- src/app/initial-balance/initial-balance.module.ts
+++ src/app/initial-balance/initial-balance.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InitialBalanceController } from './initial-balance.controller';
 import { InitialBalanceService } from './initial-balance.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { InitialBalance } from './initial-balance.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([InitialBalance])],
   controllers: [InitialBalanceController],
   providers: [InitialBalanceService]
 })
