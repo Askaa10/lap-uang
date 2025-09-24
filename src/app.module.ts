7464355861512b5557d3a70226a765ca200badb1
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2025-09-24 13:48:10 +0700

    update code

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { ExpenseModule } from './app/expense/expense.module';
 import { InitialBalanceModule } from './app/initial-balance/initial-balance.module';
 import { CloudinaryModule } from './app/cloudinary/cloudinary.module';
 import { UploadModule } from './app/upload/upload.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
