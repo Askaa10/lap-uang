4d8da49e20977f8b5e0c3c6873d69e05ee90beda
sicoding1120 <daffahafizhfirdaus07@gmail.com>
2025-09-24 16:37:37 +0700

    update code

--- src/app/cloudinary/cloudinary.service.ts
+++ src/app/cloudinary/cloudinary.service.ts
@@ -2,11 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryResponse } from './cloudinary.response';
 import streamifier from 'streamifier';
-import { File as MulterFile } from 'multer';
 
 @Injectable()
 export class CloudinaryService {
-  uploadFile(file: MulterFile): Promise<CloudinaryResponse> {
+  uploadFile(file: Express.Multer.File): Promise<CloudinaryResponse> {
     return new Promise<CloudinaryResponse>((resolve, reject) => {
       const uploadStream = cloudinary.uploader.upload_stream(
         (error, result) => {
--- src/app/upload/upload.controller.ts
+++ src/app/upload/upload.controller.ts
@@ -6,19 +6,13 @@ import {
   Param,
   Post,
   UploadedFile,
-  UploadedFiles,
-  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { JwtGuard } from '../auth/auth.guard';
-import * as fs from 'fs';
+import { FileInterceptor } from '@nestjs/platform-express';
+import multer from 'multer';
 import { CloudinaryService } from '../cloudinary/cloudinary.service';
 import { BaseResponse } from 'src/utils/response/base.response';
-import { File as MulterFile } from 'multer';
-import { File as MulterFileType } from 'multer';
-@UseGuards(JwtGuard)
+
 @Controller('upload')
 export class UploadController extends BaseResponse {
   constructor(private readonly cloudinaryService: CloudinaryService) {
@@ -26,26 +20,17 @@ export class UploadController extends BaseResponse {
   }
 
   @UseInterceptors(
-    FilesInterceptor('file', 10, {
-      storage: diskStorage({
-        destination: 'public/uploads',
-        filename: (req, file, cb) => {
-          const fileExtension = file.originalname.split('.').pop();
-          cb(null, `${new Date().getTime()}.${fileExtension}`);
-        },
-      }),
+    FileInterceptor('file', {
+      storage: multer.memoryStorage(),
       limits: {
-        fileSize: 2 * 1024 * 1024,
+        fileSize: 2 * 1024 * 1024, // 2 MB
       },
       fileFilter: (req, file, cb) => {
         const allowedTypes = ['image/jpeg', 'image/png', 'application/pdf'];
-        if (
-          !allowedTypes.includes(file.mimetype) ||
-          file.size > 2 * 1024 * 1024
-        ) {
+        if (!allowedTypes.includes(file.mimetype)) {
           return cb(
             new HttpException(
-              `Hanya file gambar dan pdf yang berukuran kurang dari 2MB yang bisa di upload, file ${file.originalname} tidak sesuai`,
+              'Hanya JPG, PNG, dan PDF yang diizinkan',
               HttpStatus.BAD_REQUEST,
             ),
             false,
@@ -55,33 +40,15 @@ export class UploadController extends BaseResponse {
       },
     }),
   )
-  @Post('file')
-  async uploadFiles(
-    @UploadedFiles() files: Array<MulterFile>,
-  ) {
-    // proses upload file
-    return {
-      message: 'Upload berhasil',
-      files,
-    };
-  }
-
-  @Delete('file/delete/:filename')
-async DeleteFile(
-    @Param('filename') filename: string,
-) {
-    try {
-        const filePath = `public/uploads/${filename}`;
-        fs.unlinkSync(filePath);
-        return { message: 'File deleted successfully' };
-    } catch (err) {
-        throw new HttpException('File not Found', HttpStatus.NOT_FOUND);
-    }
-}
-
   @Post('cloudinary')
-  async uploadImage(@UploadedFile() file: MulterFile) {
-    return this.cloudinaryService.uploadFile(file);
+  async uploadToCloudinary(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new HttpException(
+        'Tidak ada file yang diupload',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return this.cloudinaryService.uploadFile(file); // file.buffer dipakai
   }
 
   @Delete('cloudinary/delete/:public_id')
