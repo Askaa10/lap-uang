655c086b0812137f291f7d5975c3af59179d80eb
fawwaznwfl <nwflfawwaz@gmail.com>
2025-10-20 13:24:03 +0700

    be hari ini

--- src/app/upload/upload.module.ts
+++ src/app/upload/upload.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 
 @Module({
+  imports: [CloudinaryModule],
   providers: [UploadService],
   controllers: [UploadController]
 })
